ddcc0907d4d305653da7a7694791a8dce02e20ac
Karen HY Tu <karenhaoyitu@gmail.com>
2019-02-02 09:48:31 +0800

    finish assignment1

--- assignment/js/script.js
+++ assignment/js/script.js
@@ -3,12 +3,21 @@
 */
 
 function delayedResult(n1, n2, delayTime, callback){
-	let n2 = b; 
-	let n1 = a;
+// your code here
+ 	let result = n1 + n2
+ 	window.setTimeout(() => {callback(result)}, delayTime);
 }
+delayedResult(4, 5, 3000, function(result){
+console.log(result);
+});
+delayedResult(-5,10, 2000, function(result){
+window.alert(result);
+});
 
-const result = ('n1+n2');
-let delayTime = 3000; 
-window.setTimeout(() => {console.log(result+' after 3 seconds');}, delayTime);
-let delayTime2 = 2000; 
-window.setTimeout(() => {window.alert(result+' after 2 seconds');}, delayTime2);
+
+// let threesecond = ('3 seconds');
+// let delayTime = 3000; 
+// window.setTimeout(() => {console.log(threesecond);}, delayTime);
+// let twosecond = ('2 seconds');
+// let delayTime2 = 2000; 
+// window.setTimeout(() => {window.alert(twosecond);}, delayTime2);
