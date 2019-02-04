777de62e48984dde1e63378f8e3bce116021054c
Karen <karenhaoyitu@gmail.com>
2019-02-04 08:29:20 +0800

    Updates

---
+++ precache-manifest.9762e55946adf6c0fbe9493b2c8b388b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "14e93301bb86e1d59be3",
+    "url": "/week-4/assignment3/static/js/runtime~main.14e93301.js"
+  },
+  {
+    "revision": "a66fa5368a644c07f60d",
+    "url": "/week-4/assignment3/static/js/main.a66fa536.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/week-4/assignment3/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "a66fa5368a644c07f60d",
+    "url": "/week-4/assignment3/static/css/main.3c661428.chunk.css"
+  },
+  {
+    "revision": "3a00af4cec3728b38a4ee2227c8b2e3b",
+    "url": "/week-4/assignment3/index.html"
+  }
+];
